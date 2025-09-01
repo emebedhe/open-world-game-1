932fd1f8b0fd6e38bb59f7fe79a549d474d543c2
emebedhe <ashritbeskrowni@gmail.com>
2025-08-31 19:58:20 -0500

    Initial Commit

---
+++ Assets/ModularFirstPersonController/FirstPersonController/FirstPersonController.cs
@@ -0,0 +1,303 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+    using UnityEditor;
+    using System.Net;
+#endif
+
+public class FirstPersonController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    #region Camera Movement Variables
+
+    public Camera playerCamera;
+
+    public float fov = 60f;
+    public float mouseSensitivity = 2f;
+    public float maxLookAngle = 50f;
+
+    public bool lockCursor = true;
+    public bool crosshair = true;
+    public Sprite crosshairImage;
+    public Color crosshairColor = Color.white;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    private Image crosshairObject;
+    #endregion
+
+    #region Movement Variables
+
+    public float walkSpeed = 5f;
+    public float maxVelocityChange = 10f;
+
+    #region Sprint
+    public bool unlimitedSprint = false;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public float sprintSpeed = 7f;
+    public float sprintDuration = 5f;
+    public float sprintCooldown = .5f;
+    public float sprintFOV = 80f;
+    public float sprintFOVStepTime = 10f;
+
+    // Internal Variables
+    private bool isSprinting = false;
+    private float sprintRemaining;
+    private bool isSprintCooldown = false;
+    private float sprintCooldownReset;
+
+    #endregion
+
+    #region Jump
+
+    public KeyCode jumpKey = KeyCode.Space;
+    public float jumpPower = 5f;
+
+    // Internal Variables
+    private bool isGrounded = false;
+
+    #endregion
+
+    #region Crouch
+    public KeyCode crouchKey = KeyCode.LeftControl;
+    public float crouchHeight = .75f;
+    public float speedReduction = .5f;
+
+    // Internal Variables
+    private bool isCrouched = false;
+    private Vector3 originalScale;
+
+    #endregion
+    #endregion
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        crosshairObject = GetComponentInChildren<Image>();
+
+        // Set internal variables
+        playerCamera.fieldOfView = fov;
+        originalScale = transform.localScale;
+
+        if (!unlimitedSprint)
+        {
+            sprintRemaining = sprintDuration;
+            sprintCooldownReset = sprintCooldown;
+        }
+    }
+
+    void Start()
+    {
+        if(lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        if(crosshair)
+        {
+            crosshairObject.sprite = crosshairImage;
+            crosshairObject.color = crosshairColor;
+        }
+        else
+        {
+            crosshairObject.gameObject.SetActive(false);
+        }
+    }
+
+    float camRotation;
+
+    private void Update()
+    {
+        #region Camera
+        yaw = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * mouseSensitivity;
+
+
+        pitch -= mouseSensitivity * Input.GetAxis("Mouse Y");
+        pitch = Mathf.Clamp(pitch, -maxLookAngle, maxLookAngle);
+
+        transform.localEulerAngles = new Vector3(0, yaw, 0);
+        playerCamera.transform.localEulerAngles = new Vector3(pitch, 0, 0);
+        #endregion
+
+        #region Sprint
+
+            if(isSprinting)
+            {
+                playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, sprintFOV, sprintFOVStepTime * Time.deltaTime);
+
+                // Drain sprint remaining while sprinting
+                if(!unlimitedSprint)
+                {
+                    sprintRemaining -= 1 * Time.deltaTime;
+                    if (sprintRemaining <= 0)
+                    {
+                        isSprinting = false;
+                        isSprintCooldown = true;
+                    }
+                }
+            }
+            else
+            {
+                // Regain sprint while not sprinting
+                sprintRemaining = Mathf.Clamp(sprintRemaining += 1 * Time.deltaTime, 0, sprintDuration);
+            }
+
+            // Handles sprint cooldown 
+            // When sprint remaining == 0 stops sprint ability until hitting cooldown
+            if(isSprintCooldown)
+            {
+                sprintCooldown -= 1 * Time.deltaTime;
+                if (sprintCooldown <= 0)
+                {
+                    isSprintCooldown = false;
+                }
+            }
+            else
+            {
+                sprintCooldown = sprintCooldownReset;
+            }
+
+        #endregion
+
+        #region Jump
+        
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            if (isGrounded)
+            {
+                rb.AddForce(0f, jumpPower, 0f, ForceMode.Impulse);
+                isGrounded = false;
+            }
+        }
+
+        #endregion
+
+        #region Crouch
+
+            if(Input.GetKeyDown(crouchKey))
+            {
+                isCrouched = false;
+                Crouch();
+            }
+            else if(Input.GetKeyUp(crouchKey))
+            {
+                isCrouched = true;
+                Crouch();
+            }
+
+        #endregion
+
+        CheckGround();
+    }
+
+    void FixedUpdate()
+    {
+        #region Movement
+            // Calculate how fast we should be moving
+        Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        // All movement calculations while sprint is active
+        if (Input.GetKey(sprintKey) && sprintRemaining > 0f && !isSprintCooldown)
+        {
+            targetVelocity = transform.TransformDirection(targetVelocity) * sprintSpeed;
+
+            // Apply a force that attempts to reach our target velocity
+            Vector3 velocity = rb.linearVelocity;
+            Vector3 velocityChange = (targetVelocity - velocity);
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+
+            // Player is only moving when valocity change != 0
+            // Makes sure fov change only happens during movement
+            if (velocityChange.x != 0 || velocityChange.z != 0)
+            {
+                isSprinting = true;
+
+                if (isCrouched)
+                {
+                    Crouch();
+                }
+            }
+
+            rb.AddForce(velocityChange, ForceMode.VelocityChange);
+        }
+        // All movement calculations while walking
+        else
+        {
+            isSprinting = false;
+            targetVelocity = transform.TransformDirection(targetVelocity) * walkSpeed;
+
+            // Apply a force that attempts to reach our target velocity
+            Vector3 velocity = rb.linearVelocity;
+            Vector3 velocityChange = (targetVelocity - velocity);
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+
+            rb.AddForce(velocityChange, ForceMode.VelocityChange);
+        }
+
+        #endregion
+    }
+    private void CheckGround()
+    {
+        Vector3 origin = new Vector3(transform.position.x, transform.position.y - (transform.localScale.y * .5f), transform.position.z);
+        float distance = .75f;
+
+        if (Physics.Raycast(origin, transform.TransformDirection(Vector3.down), out RaycastHit hit, distance))
+        {
+            //Debug.DrawRay(origin, transform.TransformDirection(Vector3.down) * distance, Color.red);
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+
+    private void Crouch()
+    {
+        if (isCrouched)
+        {
+            transform.localScale = new Vector3(originalScale.x, originalScale.y, originalScale.z);
+            walkSpeed /= speedReduction;
+
+            isCrouched = false;
+        }
+        else
+        {
+            transform.localScale = new Vector3(originalScale.x, crouchHeight, originalScale.z);
+            walkSpeed *= speedReduction;
+
+            isCrouched = true;
+        }
+    }
+}
+
+
+
+// Custom Editor
+#if UNITY_EDITOR
+    [CustomEditor(typeof(FirstPersonController)), InitializeOnLoadAttribute]
+    public class FirstPersonControllerEditor : Editor
+    {
+    FirstPersonController fpc;
+    SerializedObject SerFPC;
+
+    private void OnEnable()
+    {
+        fpc = (FirstPersonController)target;
+        SerFPC = new SerializedObject(fpc);
+    }
+
+}
+
+#endif
