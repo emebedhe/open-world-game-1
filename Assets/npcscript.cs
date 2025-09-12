7ed525536cae6b873281d04101e1bc5235da5e51
emebedhe <ashritbeskrowni@gmail.com>
2025-09-11 21:20:59 -0500

    basic ahh dialogue lmao

--- Assets/npcscript.cs
+++ Assets/npcscript.cs
@@ -1,19 +1,37 @@
 using UnityEngine;
-
+using System.Collections.Generic;
 using TMPro;
 public class npcscript : MonoBehaviour
 {
     public GameObject text;
-
+    public List<string> Dialogues = new List<string>();
+    public GameObject inDialogue;
+    public GameObject dialogueCanvas;
 
     void Start()
     {
+        Dialogues.Add("Hello there, traveler!");
         text.SetActive(false);
     }
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.F) && text.activeSelf)
+        {
+            Debug.Log("Entering Dialogue...");
+            text.SetActive(false);
+            dialogueCanvas.SetActive(true);
+            inDialogue.SetActive(true);
+            dialogueCanvas.GetComponentInChildren<TextMeshProUGUI>().text = Dialogues[0];
+            Invoke("ExitDialogue", 3f);
+        }
     }
-
+    void ExitDialogue()
+    {
+        inDialogue.SetActive(false);
+        dialogueCanvas.SetActive(false);
+        Time.timeScale = 1f; 
+        Debug.Log("Exiting Dialogue...");
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
